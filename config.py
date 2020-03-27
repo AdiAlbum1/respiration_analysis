42ff1b166e85cb1869be0f80270b8aeda6074a75
Adi Album <adi.album1@gmail.com>
2020-03-27 23:29:16 +0300

    updated config file

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ PRESSURE_INCREASE_THRESHOLD = 2
 
 # System parameters
 SAMPLE_RATE = 50
-WINDOW_SIZE = 31
+WINDOW_SIZE = 17
 
 # Data generation parameters
 AMPLITUDE = 10
 FREQ = 0.3
 T = 20
-NOISE_SIGMA = 0.5
+NOISE_SIGMA = 0.2
 
 # Sleep time, for user experience
 # for deployment: 0
